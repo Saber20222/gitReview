ce0d3cafbf3c15ac85e6bb879980d160f000f124
Saber20222 <sabersamim.ss@gmail.com>
2022-05-31 18:35:41 -0700

    1st commit

---
+++ src/gitReview/gitReview01.java
@@ -0,0 +1,11 @@
+package gitReview;
+
+public class gitReview01 {
+
+	public static void main(String[] args) {
+		//github excercise 
+		System.out.print("today we learn github review");
+		System.out.print("today is a very good dAY review");
+	}
+
+}
